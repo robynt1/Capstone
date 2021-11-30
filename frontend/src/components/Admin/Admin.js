ff8e14328609d6e355cf3c380cd56e68a0747a7e
robynt1 <robyn6905@gmail.com>
2021-11-30 10:55:14 +0000

    chaging font size of font bar in Admin.js

--- frontend/src/components/Admin/Admin.js
+++ frontend/src/components/Admin/Admin.js
@@ -94,7 +94,7 @@ function Admin() {
   }
   return (
     <div>
-      <div class="ui mini three item menu" style={{ fontSize: "25px" }}>
+      <div class="ui mini three item menu">
         <a href="http://demo-react-spring-demo-react-spring.allstatejenkins2.conygre.com/" class="item">
           Home
         </a>
