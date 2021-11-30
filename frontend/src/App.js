4663da1fced8b858463e83bc9ed31af761fa9dbd
robynt1 <robyn6905@gmail.com>
2021-11-30 16:11:12 +0000

    updated function - toast

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,42 +3,32 @@ import logo from './logo.svg';
 import Create from './components/Create/Create';
 import Admin from './components/Admin/Admin';
 import Welcome from './components/Welcome/Welcome';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom' 
 import './App.css';
 
 
 function App() {
-
+  
   return (
+    
+      <BrowserRouter >
+      
+        <div className="App">
+  
+          <div>
+            <Route exact path="/" component={Welcome} />
+          </div>
 
-    <BrowserRouter >
-      <ToastContainer
-        position="bottom-left"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-      <div className="App">
+           <div>
+            <Route exact path="/Admin" component={Admin} />
+          </div>
 
-        <div>
-          <Route exact path="/" component={Welcome} />
+          <div>
+            <Route exact path="/Create" component={Create} />
+          </div>
+        
         </div>
-
-        <div>
-          <Route exact path="/Admin" component={Admin} />
-        </div>
-
-        <div>
-          <Route exact path="/Create" component={Create} />
-        </div>
-
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
   );
 }
 
